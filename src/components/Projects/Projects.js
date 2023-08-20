f757310906531e35a097ea14fc0219b5f70840e1
Jacks-London <jacklondon0430@gmail.com>
2023-08-20 18:21:50 +0900

    new version

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -67,7 +67,6 @@ function Projects() {
               demoLink="http://www.houseofcoco.co.uk/"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={businessfriend}
@@ -77,9 +76,6 @@ function Projects() {
               demoLink="https://www.businessfriend.com/"
             />
           </Col>
-
-          
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={WordPress}
@@ -109,54 +105,6 @@ function Projects() {
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={shopifyglasses}
-              isBlog={false}
-              title="LA LENTERiA"
-              description=""
-              demoLink="https://lalenteria.mx/"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Casino}
-              isBlog={false}
-              title="Casino"
-              description=""
-              demoLink="https://lasatlantisbonuses.com"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Fitness}
-              isBlog={false}
-              title="Fitness"
-              description=""
-              demoLink="http://www.centralpennfitnesscenter.com/"
-            />
-          </Col>
-          
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Game}
-              isBlog={false}
-              title="Game"
-              description=""
-              demoLink="http://stardewvalley.net/"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={systemagency}
-              isBlog={false}
-              title="System of agency"
-              description=""
-              demoLink="https://www.systemagency.com"
-            />
-          </Col>
-
-
-          <Col md={4} className="project-card">
-            <ProjectCard
               imgPath={suicide}
               isBlog={false}
               title="Ai For Social Good"
@@ -164,7 +112,6 @@ function Projects() {
               ghLink="https://github.com/Jacks-London/Social_Good"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
@@ -181,9 +128,53 @@ function Projects() {
               isBlog={false}
               title="PLANT-AI [Recognition of Plant Diseases by Leaf Image Classification]"
               description="Food security for billions of people on earth requires minimizing crop damage by timely detection of diseases.Developing methods for detection of plant diseases serves the dual purpose of increasing crop yield and reducing pesticide use without knowing about the proper disease. Along with development of better crop varieties, disease detection is thus paramount goal for achieving food security. The traditional method of disease detection has been to use manual examination by either farmers or experts, which can be time consuming and costly, proving infeasible for millions of small and medium sized farms around the world.
-
               This project is an approach to the development of plant disease recognition model, based on leaf image classification, by the use of deep convolutional networks. The developed model is able to recognize 38 different types of plant diseases out of of 14 different plants with the ability to distinguish plant leaves from their surroundings."
               ghLink="https://github.com/Jacks-London/AI_Project_2"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Casino}
+              isBlog={false}
+              title="Casino"
+              description="I have developed a casino website that offers a wide range of popular casino games such as slots, poker, and blackjack. The website features a user-friendly interface, secure payment methods, and attractive bonuses and promotions for players. The platform also provides 24/7 customer support to ensure a seamless gaming experience for users."
+              demoLink="https://lasatlantisbonuses.com"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={systemagency}
+              isBlog={false}
+              title="System of agency"
+              description=""
+              demoLink="https://www.systemagency.com"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Game}
+              isBlog={false}
+              title="Game"
+              description=""
+              demoLink="http://stardewvalley.net/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={shopifyglasses}
+              isBlog={false}
+              title="LA LENTERiA"
+              description=""
+              demoLink="https://lalenteria.mx/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Fitness}
+              isBlog={false}
+              title="Fitness"
+              description=""
+              demoLink="http://www.centralpennfitnesscenter.com/"
             />
           </Col>
         </Row>
