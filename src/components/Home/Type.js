ce1fc920c98caa1158bf10998aec55ac7f7cb0fe
Jacks-London <jacklondon0430@gmail.com>
2023-08-22 18:09:29 +0900

    reverse

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,16 +6,12 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Dear Henry",
-          "Can you get in touch with me on Skype?",
-          "You can check my skype id on resume page.",
-          "If you help me with that, I will do my best."
-          // "Software Developer",
-          // "Freelancer",
-          // "Web Developer",
-          // "Web Application Developer",
-          // "ArcGIS Expert",
-          // "Autodesk AutoCAD Expert"
+          "Software Developer",
+          "Freelancer",
+          "Web Developer",
+          "Web Application Developer",
+          "ArcGIS Expert",
+          "Autodesk AutoCAD Expert"
         ],
         autoStart: true,
         loop: true,
